5030447e6c00094939f95dac897513c08a9d2e3f
Vinícius Oliveira <me@vinioliveira.com>
2016-01-06 21:59:11 -0200

    Including match layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'game/index'
+
   devise_for :players
 
   resources :players
   get 'standings/overall', to: 'standings#overall'
   get 'scores/:season/:week', to: 'scores#index'
+  get 'match', to: 'match#index'
 
   root 'home#index'
 end
