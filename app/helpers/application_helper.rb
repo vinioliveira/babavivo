ac5a8c36a3c06381361ae4fb56033ed990630eaf
Vinícius Oliveira <me@vinioliveira.com>
2016-01-09 18:18:00 -0200

    removing useless code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,6 @@ module ApplicationHelper
     case level.to_sym
     when :success then "ui green message"
     when :error then "ui red message"
-    when :notice then "ui blue message"
     end
   end
 end
