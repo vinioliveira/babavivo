7a77c1ecd521b2451a80e4aadfb0cfeb04ce6d3a
Vinícius Oliveira <me@vinioliveira.com>
2016-03-20 12:22:04 -0300

    Adding match start to create a new match

--- app/models/match.rb
+++ app/models/match.rb
@@ -4,6 +4,13 @@ class Match < ActiveRecord::Base
   belongs_to :weekly_standing, inverse_of: :matchs
   has_one :match_report, inverse_of: :match
 
+  def self.start(attrs)
+    if current_match = current
+      current_match
+    else
+      create(attrs)
+    end
+  end
 
   def self.current
     find_by(ends_at: nil)
--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -19,4 +19,47 @@ RSpec.describe Match, type: :model do
       end
     end
   end
+
+  describe '.start' do
+    let(:first_team) { create(:team, color: 'red', name: 'Team 1') }
+    let(:second_team) { create(:team, color: 'green', name: 'Team 1') }
+    let(:weekly_standing) { create(:weekly_standing) }
+
+    let(:attrs) do
+      {
+        first_team: first_team,
+        second_team: second_team,
+        weekly_standing: weekly_standing
+      }
+    end
+
+    it 'returns a new instance of current match' do
+      expect(described_class.start(attrs)).to be_a(Match)
+    end
+
+    it 'creates a new match' do
+      expect { described_class.start(attrs) }.to change {
+        described_class.count
+      }.from(0).to(1)
+    end
+
+    it 'creates a new match with two team' do
+      match = described_class.start(attrs)
+      expect(match.first_team).to eql(first_team)
+      expect(match.second_team).to eql(second_team)
+    end
+
+    context 'exists un current match' do
+      let!(:unfinisehd_matchs) { create(:match) }
+
+      it 'does not create a new match if exists a current match' do
+        expect { described_class.start(attrs) }.not_to change {
+          described_class.count
+        }.from(1)
+      end
+      it 'returns the current match if exists on active' do
+        expect(described_class.start(attrs)).to eql(unfinisehd_matchs)
+      end
+    end
+  end
 end
