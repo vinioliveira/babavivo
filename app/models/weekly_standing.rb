3d45ac1590fdca9c8ff914f82bddb663fb14df58
Vinícius Oliveira <me@vinioliveira.com>
2016-03-20 13:21:42 -0300

    Adding current week to weekly_standing

--- app/models/weekly_standing.rb
+++ app/models/weekly_standing.rb
@@ -2,4 +2,13 @@ class WeeklyStanding < ActiveRecord::Base
   has_many :matchs, inverse_of: :weekly_standing
   has_many :standings, inverse_of: :weekly_standing
   has_many :players, through: :standings
+
+  def self.current
+    if last_weekly_standing = last
+      if last_weekly_standing.created_at.to_date.cweek == DateTime.current.cweek
+        return last_weekly_standing
+      end
+    end
+    create
+  end
 end
--- spec/models/weekly_standing_spec.rb
+++ spec/models/weekly_standing_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe WeeklyStanding, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to have_many(:matchs).inverse_of(:weekly_standing) }
+    it { is_expected.to have_many(:standings).inverse_of(:weekly_standing) }
+    it { is_expected.to have_many(:players).through(:standings) }
+  end
+
+  describe 'current' do
+    it 'returns an instance of weekly_standing' do
+      expect(described_class.current).to be_a(WeeklyStanding)
+    end
+
+    it 'creates a new weekly_standing' do
+      expect{ described_class.current }.to change{ described_class.count }.from(0).to(1)
+    end
+
+    it 'returns the weekly_standing of the current week' do
+      current_weekly_standing = create(:weekly_standing, :complete, round: 1)
+      expect(described_class.current).to eql(current_weekly_standing)
+    end
+
+    it 'creates the weekly_standing of the current week if doesnt exist' do
+      old_current = create(:weekly_standing, :complete, round: 1)
+      travel 2.weeks do
+        expect{ described_class.current }.to change{ described_class.count }.from(1).to(2)
+        expect(described_class.current).not_to eql(old_current)
+      end
+    end
+  end
 end
